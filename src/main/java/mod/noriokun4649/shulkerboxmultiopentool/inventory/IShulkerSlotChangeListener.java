2d0b394e82c02926e898bfa4463840e8718d5996
noriokun4649 <noriokun4649@outlook.com>
2020-10-09 10:23:34 +0900

    インターフェースの見直し

--- src/main/java/mod/noriokun4649/shulkerboxmultiopentool/inventory/IShulkerSlotChangeListener.java
+++ src/main/java/mod/noriokun4649/shulkerboxmultiopentool/inventory/IShulkerSlotChangeListener.java
@@ -1,11 +1,10 @@
 package mod.noriokun4649.shulkerboxmultiopentool.inventory;
 
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
 
 public interface IShulkerSlotChangeListener {
 
-    void addShulkerBoxOnSlot(IInventory inventory, ItemStack slot1, ItemStack slot2, ItemStack slot3);
+    void addShulkerBoxOnSlot(IInventory inventory, int slotNum);
 
     void removeShulkerBoxOnSlot(IInventory iInventory, int slotNum);
 
